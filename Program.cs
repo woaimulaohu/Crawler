8d9f03a5a055883ad18270e13a91316b9dea6421
兔斯基斯基 <11761740@qq.com>
2018-04-10 22:49:48 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                 .UseKestrel()           // 指定WebServer为Kestrel
+                 .UseStartup<Startup>()  // 配置WebHost
+                 .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace crawler
+{
+    internal class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            Console.WriteLine("app start");
+            app.Run(c => {
+                return c.Response.WriteAsync("Hello dotnet core.");
+            });
+        }
+    }
+}
